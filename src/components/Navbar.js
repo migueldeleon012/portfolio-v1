dc9a465e7efc659327129fcd32d88f8da3561991
Miguel de Leon <miguelangelodeleon.12@gmail.com>
2021-09-29 10:29:26 +0200

    feat: add dummy links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,27 +8,27 @@ const Navbar = () => {
       </div>
       <ul>
         <li>
-          <a className="text-white" href="">
+          <a className="text-white" href="#">
             About Me
           </a>
         </li>
         <li>
-          <a className="text-white" href="">
+          <a className="text-white" href="#">
             Skills
           </a>
         </li>
         <li>
-          <a className="text-white" href="">
+          <a className="text-white" href="#">
             Resume
           </a>
         </li>
         <li>
-          <a className="text-white" href="">
+          <a className="text-white" href="#">
             Projects
           </a>
         </li>
         <li>
-          <a className="text-white" href="">
+          <a className="text-white" href="#">
             Contact
           </a>
         </li>
