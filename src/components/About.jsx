70235c1035c95d206a1afd07cb0400ace17776a8
Miguel de Leon <miguelangelodeleon.12@gmail.com>
2022-08-16 13:09:19 +0200

    feat: updated about me paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,10 @@ const About = () => {
         <div className="section__container__desc">
           <p>
             Hi there! I’m Miguel de Leon, a full-stack Web Developer with a
-            passion for Front-end and Design. I’m currently attending an online
-            coding bootcamp, and will graduate soon at Uplift Code Camp based in
-            the Philippines, but I am currently living in Italy. I aspire toward
-            a career that will allow me to challenge myself through learning new
-            technologies.
+            passion for Front-end and Design. I enrolled at Uplift Code Camp, a
+            coding bootcamp based in the Philippines, and graduated but I am
+            currently living in Italy. I aspire toward a career that will allow
+            me to challenge myself through learning new technologies.
           </p>
           <p>
             When I’m not developing websites I enjoy biking, working out,
